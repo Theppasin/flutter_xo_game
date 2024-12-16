dbd934dee60dfc6e0cf9cf876a4e179308aebb79
Sin Theppasin <tappasin.kongsakda@gmail.com>
2024-12-16 15:55:08 +0700

    init: Initialize project

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_xo_game
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/theppasin/flutter_xo_game/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.theppasin.flutter_xo_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
